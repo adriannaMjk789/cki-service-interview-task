0bcf9bba3dd01fca83820236e6105e229033eb6c
Adrianna Majewska <majewskaa@booxware.de>
2025-03-17 19:29:10 +0100

    chore: add missing file for code review

--- src/test/java/com/circlek/Test01.java
+++ src/test/java/com/circlek/Test01.java
@@ -10,7 +10,7 @@ import static org.testng.Assert.assertEquals;
 
 public class Test01 {
 
-    //CODE REVIEW
+    //CODE REVIEW - By Adrianna
     @Test
     public void assertTextOnPage(){
         System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
